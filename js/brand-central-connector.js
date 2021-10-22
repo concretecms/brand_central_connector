bbde23ef9278f53240b52e994b5da909a2ef7430
Andrew Embler <andrew@concrete5.org>
2021-10-22 08:12:30 -0700

    bug fixes for if this is ever included on a page for a user without admin rights

--- js/brand-central-connector.js
+++ js/brand-central-connector.js
@@ -1,27 +1,29 @@
 (function ($) {
     $(function () {
         // custom import handler
-        ConcreteEvent.subscribe('ExternalFileProvider.SelectFile', function (e, data) {
-            // only catch events for the brand_central connector
-            if (data.externalFileProviderTypeHandle === "brand_central") {
-                jQuery.fn.dialog.open({
-                    title: data.externalFileProviderName,
-                    href: CCM_DISPATCHER_FILENAME + "/ccm/brand_central_connector/select_asset_file/" + data.externalFileProviderId + "/" + data.selectedFile.fID + "?externalFileProviderUploadDirectoryId=" + encodeURI(data.externalFileProviderUploadDirectoryId),
-                    width: '80%',
-                    modal: true,
-                    height: '500',
-                    onOpen: function () {
-                        ConcreteEvent.subscribe('AjaxFormSubmitSuccess', function (e, data) {
-                            ConcreteEvent.publish('FileManagerSelectFile', {
-                                fID: [data.response.importedFileId]
+        if (typeof ConcreteEvent !== 'undefined') {
+            ConcreteEvent.subscribe('ExternalFileProvider.SelectFile', function (e, data) {
+                // only catch events for the brand_central connector
+                if (data.externalFileProviderTypeHandle === "brand_central") {
+                    jQuery.fn.dialog.open({
+                        title: data.externalFileProviderName,
+                        href: CCM_DISPATCHER_FILENAME + "/ccm/brand_central_connector/select_asset_file/" + data.externalFileProviderId + "/" + data.selectedFile.fID + "?externalFileProviderUploadDirectoryId=" + encodeURI(data.externalFileProviderUploadDirectoryId),
+                        width: '80%',
+                        modal: true,
+                        height: '500',
+                        onOpen: function () {
+                            ConcreteEvent.subscribe('AjaxFormSubmitSuccess', function (e, data) {
+                                ConcreteEvent.publish('FileManagerSelectFile', {
+                                    fID: [data.response.importedFileId]
+                                });
+
+                                ConcreteEvent.unsubscribe('AjaxFormSubmitSuccess');
                             });
+                        }
+                    });
 
-                            ConcreteEvent.unsubscribe('AjaxFormSubmitSuccess');
-                        });
-                    }
-                });
-
-            }
-        });
+                }
+            });
+        }
     });
 })(jQuery);
